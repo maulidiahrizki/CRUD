28cb7ea7c2173ca68d181f404d5c6a7b43280e6a
maulidiahrizki <lidiamarisya13@gmail.com>
2023-10-17 14:02:51 +0700

    laporan ke 18

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors())
 
 //membuat route baru dengan method GET yang isinya kalimat halo dek
 // app.get('/', (req,res) => {
